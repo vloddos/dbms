2b753d9576fd435b36ab7606df0391f2cc29d891
Kolot Yuri <kolot.ius@students.dvfu.ru>
2018-12-05 19:34:39 +1000

    SE coverage now 51%

--- src/test/java/com/dbms/structs/DataBaseTest.java
+++ src/test/java/com/dbms/structs/DataBaseTest.java
@@ -1,21 +1,38 @@
 package com.dbms.structs;
 
-import com.dbms.storage.StorageEngine;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.LinkedHashMap;
 
 public class DataBaseTest {
 
-    @Test(expected = Exception.class)
-    public void testShouldThrowExceptionCauseDatabaseDoesntExists() throws Exception {
-        DataBases.getInstance().getCurrentDataBase().getTable("City");
+     private DataBase testDB = new DataBase("School");
+
+    @Test
+    public void testShouldReturnDatabaseName() {
+        Assert.assertEquals("Films", new DataBase("Films").getName());
     }
 
     @Test(expected = Exception.class)
-    public void testShouldThrowExceptionDatabase() throws Exception {
-        StorageEngine.getInstance().init();
+    public void testShouldThrowExceptionCauseTableDoesntExist() throws Exception {
+        testDB.getTable("Student");
+    }
 
-        DataBases.getInstance().fullLoad();
-        DataBases.getInstance().createDataBase("School");
-        DataBases.getInstance().getDataBase("School");
+    @Test(expected = Exception.class)
+    public void testShouldThrowExceptionCauseTableAlreadyExists() throws Exception {
+        var map = new LinkedHashMap<String, TypeDescription>();
+
+        map.put("age", new TypeDescription("int"));
+        map.put("field", new TypeDescription("bool"));
+
+        testDB.createTable("Class", map);
+        testDB.createTable("Class", map);
+    }
+
+    //FIXME(RoyalStorm): table exist, but access denied
+    @Test
+    public void testShouldReturnTableName() throws Exception {
+        Assert.assertNotNull(testDB.getTable("Class"));
     }
 }
